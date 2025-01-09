a8604cbad91b02a1c6cd5b5910098db16f327a6a
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-01-09 17:22:19 -0500

    Add files via upload

--- R/motifs.R
+++ R/motifs.R
@@ -1,14 +1,42 @@
 
 
-#' Annotations
+
+#' Call mean
 #'
-#' @param object anno
-#' @param motif ids
+#' @param data frame
 #' @param group name
 #' @return data frame
 #' @export
 #'
-CalMeanMotifSig <- function(seu, motifs='all', group='')
+CallMeanByGroup <- function(df, group='')
+{ 
+    # remove group name
+    col_name <- colnames(df)
+    col_name <- col_name[ !col_name == group]
+
+    # call means
+    df <- df %>% 
+            group_by(.data[[group]]) %>%
+            summarise(across(col_name, mean))
+
+    df <- as.data.frame(df)
+
+    return(df)
+}
+
+
+
+
+#' Calculate mean motif signal
+#'
+#' @param object anno
+#' @param motif ids
+#' @param group name
+#' @param split by sample name yes/no
+#' @return data frame
+#' @export
+#'
+CalMeanMotifSig <- function(seu, motifs='all', group='', split=FALSE)
 {   
     d_motif <- NULL
 
@@ -24,17 +52,35 @@ CalMeanMotifSig <- function(seu, motifs='all', group='')
     metadata <- seu@meta.data
     d_motif[group] <- metadata[[group]][match(rownames(metadata), rownames(d_motif))]
 
-    # remove group name
-    col_name <- colnames(d_motif)
-    col_name <- col_name[ !col_name == group]
+    # calculare
+    d_avg_score <- NULL
 
-    d_motif <- d_motif %>% 
-            group_by(.data[[group]]) %>%
-            summarise(across(col_name, mean))
+    if (split){
+        print(table(metadata$orig.ident))
 
-    return(d_motif)
+        sampleset <- unique(metadata$orig.ident)
+        i <- 1
+        for (s in sampleset){
+            cells <- rownames(metadata[metadata$orig.ident==s,])
+            dsub <- d_motif[cells,]
+            print(paste0(s, ':', nrow(dsub)))
+            dsub_score <- CallMeanByGroup(dsub, group)
+            dsub_score$subgroup <- s
+
+            if (i == 1){
+                d_avg_score <- dsub_score
+                i = 2
+            } else {
+                d_avg_score <- rbind(d_avg_score, dsub_score)
+            }
+        }
+        
+    } else {
+        d_avg_score <- CallMeanByGroup(d_motif, group)
+    }
+    
+    return(d_avg_score)
 }
-
 
 
 
