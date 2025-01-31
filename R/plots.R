a8969e6ac7c8d9d6dc0721b620f72a4ac14191f4
Hua Sun <33794673+hsun9@users.noreply.github.com>
2025-01-31 16:41:21 -0500

    Add files via upload

--- R/plots.R
+++ R/plots.R
@@ -239,9 +239,9 @@ Seurat_DimPlot2 <- function(obj=NULL,
                 panel.grid.minor = element_blank(),
                 axis.line = element_blank()) +
             # x
-            annotation_custom(grob = grid::linesGrob(), xmin = xmin*1.5, xmax = 0.8, ymin = ymin*1.3, ymax = ymin*1.3) +
+            annotation_custom(grob = grid::linesGrob(), xmin = xmin*1.3, xmax = xmin + abs(xmin)*0.8, ymin = ymin*1.15, ymax = ymin*1.15) +
             # y
-            annotation_custom(grob = grid::linesGrob(), xmin = xmin*1.5, xmax = xmin*1.5, ymin = ymin*1.3, ymax = 0) +
+            annotation_custom(grob = lgrid::inesGrob(), xmin = xmin*1.3, xmax = xmin*1.3, ymin = ymin*1.15, ymax = ymin + abs(ymin)*0.8) +
             coord_cartesian(xlim=c(xmin, xmax), ylim = c(ymin, ymax), clip = "off") +
             theme(axis.title = element_text(hjust = 0))
 
